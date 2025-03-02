7a57d5b4ff577c56e478844c6d17ea257dc8af61
Soubhagya Kumar Mishra <240720100036@centurionuniv.edu.in>
2025-03-03 00:14:45 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,17 @@
+public class AddNumbers {
+    int a,b,sum;
+    AddNumbers(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+        sum = a+b;
+    }
+    public void displaySum()
+    {
+        System.out.println("Sum = "+sum);
+    }
+    public static void main(String[] args) {
+        AddNumbers obj = new AddNumbers(30,43);
+        obj.displaySum();
+    }
+}
