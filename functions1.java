f69861c89a4f42c1181a50f477d9cf8e305e2d22
Soubhagya Kumar Mishra <240720100036@centurionuniv.edu.in>
2025-03-10 08:54:17 +0530

    Add files via upload

---
+++ functions1.java
@@ -0,0 +1,11 @@
+public class functions1{
+    public static int addition(int a,int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 37;
+        System.out.println(addition(a, b));
+    }
+}
