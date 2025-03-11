82ec5eae06df78581e5819b8f3149afdd472573e
Soubhagya Kumar Mishra <240720100036@centurionuniv.edu.in>
2025-03-11 22:52:49 +0530

    Add files via upload

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 public class fibonacci {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of series : ");
-        int n = sc.nextInt();
+    public static class Fibonacciseries {
+        void display() {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter 1st interval ");
+            int m = sc.nextInt();
+            
+            System.out.print("Enter 2nd Interval : ");
+            int n = sc.nextInt();
+    
+            int a = 0;
+            int b = 1;
+            int next;
+    
+            for(int i = 0;i<=n;i++)
+            {
+                if(a >=m && a <=n){
+                    System.out.print(a+" ");
+                }
+                next = a+b;
+                a = b;
+                b = next;
+            }
 
-        int a = 0;
-        int b = 1;
-        int next;
-
-        for(int i = 1;i<=n;i++)
-        {
-            System.out.print(a+" ");
-            next = a+b;
-            a = b;
-            b = next;
         }
+    }
+    public static void main(String[] args) {
+       Fibonacciseries obj = new Fibonacciseries();
+       obj.display();
     }
 }
