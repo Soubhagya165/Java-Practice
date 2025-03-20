0ccd9bab250c25b1d2380329c419b976a7fb081f
Soubhagya Kumar Mishra <240720100036@centurionuniv.edu.in>
2025-03-20 09:54:12 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,16 @@
+class Node{
+    int data;
+    int address;
+}
+
+public class Address {
+    public static void main(String[] args) {
+        
+    
+    Node obj = new Node();
+    obj.data = 10;
+    obj.address = 10;
+    System.out.println();
+    System.out.println();
+    }
+}
