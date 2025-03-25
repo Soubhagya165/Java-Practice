35f25f975227e9e264762762b17923283a35a316
Soubhagya Kumar Mishra <240720100036@centurionuniv.edu.in>
2025-03-25 21:57:16 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String arr[]= new String[5];
+        String arr1[]= new String[5];
         System.out.print("Enter array elements : ");
         for(int i = 0;i<arr.length;i++)
         {
-            arr[i] = sc.next();
+            arr1[i] = sc.next();
         }
         for(int i = 0;i<arr.length;i++)
         {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr1[i]+" ");
         }
         
     }
